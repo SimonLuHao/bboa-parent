ca9b09b4e9a9b1dc72ea79f7fcf364a716db9ce2
simon <simonluhao@sina.com>
2017-07-15 22:25:09 +0800

    离开家

--- src/main/java/com/simon/consumer/TestConsumer.java
+++ src/main/java/com/simon/consumer/TestConsumer.java
@@ -6,10 +6,10 @@ import com.alibaba.rocketmq.client.consumer.DefaultMQPullConsumer;
 import com.alibaba.rocketmq.client.exception.MQClientException;
 
 public class TestConsumer {
-
+	//kasjdflakjsdflakjdflakjsdflakjsdf
+	//浜嗚В绂诲紑瀹朵簡瑙佽皡瑙佽皡鐪嬭浜嗕簡浜ゆ祦绌洪棿绂诲紑灏�
 	public static void main(String[] args) throws MQClientException{
-		DefaultMQPullConsumer consumer =
-new DefaultMQPullConsumer();
+		DefaultMQPullConsumer consumer =new DefaultMQPullConsumer();
 		consumer.createTopic(UUID.randomUUID().toString(), "SIMON", 1);
 		consumer.setNamesrvAddr("127.0.0.1:9876");
 		consumer.setConsumerGroup("fantuantuan");
